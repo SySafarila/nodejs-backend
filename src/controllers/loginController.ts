fb5d344ea3aa75326f583a4f0465e84407b90e49
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2024-07-21 20:00:31 +0700

    compare password with bcrypt

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
 import { Request, Response } from "express";
 import Joi from "joi";
 import CustomError from "../CustomError";
@@ -19,25 +20,29 @@ const loginController = async (req: Request, res: Response) => {
 
     await schema.validateAsync({ email, password } as LoginType, options);
 
-    const checkEmail = await prisma.user.findFirst({
+    const findUser = await prisma.user.findFirst({
       where: {
         email: email,
       },
     });
 
-    if (!checkEmail) {
-      // throw new Error("User not found");
+    if (!findUser) {
       throw new CustomError("User not found", 404);
     }
 
-    const comparePassword = "";
-    if (checkEmail.password != comparePassword) {
-      // throw new Error("Credentials not match");
+    const comparePassword: boolean = await bcrypt.compare(
+      password,
+      findUser.password
+    );
+    if (!comparePassword) {
       throw new CustomError("Credentials not match", 401);
     }
 
+    const token = "TOKEN";
+
     res.json({
-      message: "Hello from Express Typescript",
+      message: "Login success",
+      token: token,
     });
   } catch (error: any) {
     if (error instanceof Joi.ValidationError) {

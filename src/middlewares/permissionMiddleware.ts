51158161b135b6b6a5d072be251701a967e31bf8
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2024-07-28 13:25:15 +0700

    update permission middleware

--- src/middlewares/permissionMiddleware.ts
+++ src/middlewares/permissionMiddleware.ts
@@ -3,6 +3,13 @@ import SignedResponseType from "../types/SignedResponseType";
 
 const permissionMiddleware = (permission: string) => {
   return async (req: Request, res: SignedResponseType, next: NextFunction) => {
+    const checkRole = res.locals.roles?.includes("super-admin");
+
+    if (checkRole) {
+      next();
+      return;
+    }
+
     const checkPermission = res.locals.permissions?.includes(permission);
 
     if (!checkPermission) {

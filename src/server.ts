45d427d39901b73a48f1b2df81aab4db7643628c
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2024-08-03 20:35:41 +0700

    install cors

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from "express";
 import rootController from "./controllers/rootController";
 import auth from "./routes/auth";
@@ -7,6 +8,7 @@ import roles from "./routes/roles";
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", rootController);
 app.use(auth);

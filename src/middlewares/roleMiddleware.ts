813da2ca7f1bf91eff67ffe8248873e906b1fbe1
Syahrul Safarila <sysafarila.official@gmail.com>
2024-08-06 09:20:14 +0700

    update middleware

--- src/middlewares/roleMiddleware.ts
+++ src/middlewares/roleMiddleware.ts
@@ -4,6 +4,12 @@ import SignedResponseType from "../types/SignedResponseType";
 const roleMiddleware = (role: string) => {
   return async (req: Request, res: SignedResponseType, next: NextFunction) => {
     const checkRole = res.locals.roles?.includes(role);
+    const checkSuperAdmin = res.locals.roles?.includes("super-admin");
+
+    if (checkSuperAdmin) {
+      next();
+      return;
+    }
 
     if (!checkRole) {
       res.status(403).json({ message: "You don't have right role" });

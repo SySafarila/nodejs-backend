e6e67918a126ecb9d75a0a7ec88652a8c6839ca6
Syahrul Safarila <sysafarila.official@gmail.com>
2024-10-02 16:54:45 +0700

    add semver

---
+++ semver.ts
@@ -0,0 +1,11 @@
+import semver from "semver";
+
+const minimum_node_version = "v20.17.0";
+const current_node_version = process.version;
+
+if (!semver.satisfies(current_node_version, minimum_node_version)) {
+  console.log(
+    `minimum node version is ${minimum_node_version}, your current node version is ${current_node_version}`
+  );
+  process.exit(1);
+}

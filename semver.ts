57647e7ea6ce6ca1c1ec6d655fa99c9841f42db6
Syahrul Safarila <sysafarila.official@gmail.com>
2024-10-02 17:11:31 +0700

    update package.json

--- semver.ts
+++ semver.ts
@@ -1,6 +1,6 @@
 import semver from "semver";
 
-const minimum_node_version = "v20.17.0";
+const minimum_node_version = ">=v20.17.0";
 const current_node_version = process.version;
 
 if (!semver.satisfies(current_node_version, minimum_node_version)) {
